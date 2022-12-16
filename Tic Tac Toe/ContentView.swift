c28152351eb027c97db43455ff8eecaf587fb737
oliver halvey <oliverhalvey310@yahoo.com>
2022-12-16 07:33:12 -0800

    Finished Game

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,19 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Tic-Tac-Toe")
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        
+                        Color.white
+                            .opacity(moves[index] ==  "" ? 1 : 0)
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                            }
+                        }
+                    }
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] != "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0)
+                    )
+                }
+                .preferredColorScheme(.dark)
+                .alert(isPresented: $gameOver) {
+                    Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play Again!"),
+                                                                               action: {
+                        withAnimation {
+                            moves = Array(repeating: "", count: 9)
+                            gameOver = false
+                        }
+                    }))
+                }
+                .onChange(of: moves) { newValue in
+                    checkForWinner()
+                    if !(gameOver || moves.contains("")){
+                        winMessage = "Cat's Game!"
+                        gameOver = true
+                    }
+                }
+                .padding()
+            }
         }
-        .padding()
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    private func checkForWinner() {
+        checkLine(a: 0, b: 1, c: 2)
+        checkLine(a: 3, b: 4, c: 5)
+        checkLine(a: 6, b: 7, c: 8)
+        checkLine(a: 0, b: 4, c: 8)
+        checkLine(a: 6, b: 4, c: 2)
+        checkLine(a: 0, b: 3, c: 6)
+        checkLine(a: 1, b: 4, c: 7)
+        checkLine(a: 2, b: 5, c: 8)
+        
+    }
+    
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            
+            winMessage = "\(moves[a]) has won!"
+            gameOver = true
+            
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
